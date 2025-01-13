62e1befbf57afcfb805d1394048543b59d1663c8
BoyChai <1972567225@qq.com>
2025-01-13 18:03:13 +0800

    refactor: remove unused args check in main

--- main.go
+++ main.go
@@ -12,13 +12,9 @@ func main() {
 	p := flag.String("p", "./", "指定路径,默认为当前路径,匹配方式是这个路径下的全部evtx文件")
 	o := flag.String("o", "./event.xlsx", "输出位置,默认为当前目录的 event.xlsx")
 	c := flag.Bool("c", false, "输出模式,默认为false 追加")
+
 	flag.Parse()
 
-	args := flag.Args()
-	if len(args) < 1 {
-		fmt.Println("传入参数有误，请查看使用说明")
-		return
-	}
 	switch *t {
 	case "p":
 		control.Cfg.Path = *p
