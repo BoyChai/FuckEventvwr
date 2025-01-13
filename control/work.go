ce13618b7eb096595ecde18ba96f5865bcd77912
BoyChai <1972567225@qq.com>
2025-01-13 18:35:47 +0800

    feat: implement work module with file, chunk, and record handling

--- control/work.go
+++ control/work.go
@@ -1,6 +1,72 @@
 package control
 
-func Run() {
-	// TODO: implement
+import (
+	"FuckEventvwr/velocidex/evtx"
+	"sync"
+)
+
+// 锁
+var filesLock sync.Mutex
+var chunksLock sync.Mutex
+var recordsLock sync.Mutex
+
+// 所有文件
+var files []string
+
+// 所有chunk
+var chunks []*evtx.Chunk
+
+// 所有事件
+var records []*evtx.EventRecord
+
+func takeFile() string {
+	filesLock.Lock()
+	defer filesLock.Unlock()
+
+	if len(files) == 0 {
+		return ""
+	}
+	file := files[0]
+	files = files[1:]
+	return file
+}
+
+func takeChunk() *evtx.Chunk {
+	chunksLock.Lock()
+	defer chunksLock.Unlock()
+
+	if len(chunks) == 0 {
+		return nil
+	}
+	chunk := chunks[0]
+	chunks = chunks[1:]
+	return chunk
+}
+
+func takeRecord() *evtx.EventRecord {
+	recordsLock.Lock()
+	defer recordsLock.Unlock()
+
+	if len(records) == 0 {
+		return nil
+	}
+	record := records[0]
+	records = records[1:]
+	return record
+}
+
+func fileWork() {
+
+}
+
+func chunkWork() {
+
+}
+
+func recordWork() {
+
+}
+
+func writeWork() {
 
 }
